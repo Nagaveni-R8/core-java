1778e8ae522423611f8b8039603cc88d44d2e88b
Nagaveni-R8 <nagaveni.xworkz@gmail.com>
2024-06-24 11:17:59 +0530

    sgghgs

---
+++ Projector.java
@@ -0,0 +1,14 @@
+class Projector {
+
+    public static void main (String[] Epson){
+	
+	System.out.println("Projector is an optical device");
+	System.out.println("Projector is a device used to display images");
+	System.out.println("Projectors are great for watching movies on a big screen");
+	System.out.println("Projectors can be used for outdoor events");
+	System.out.println("Projector is an optical device that projects an image onto a surface");
+	
+	
+	}
+
+}
